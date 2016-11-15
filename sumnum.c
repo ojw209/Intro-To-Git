e4156b1ca5559efcd49c14d68bf7ae158c06bb89
ojw209 <ojw209@exeter.ac.uk>
2016-11-15 14:17:29 +0000

    Now Checks if Odd.

--- sumnum.c
+++ sumnum.c
@@ -8,6 +8,17 @@ scanf("%d %d", &n1, &n2);
 // store sum in variable Sum
 Sum = n1 + n2;
 // display output
-printf("%d + %d = %d", n1, n2, Sum);
+printf("%d + %d = %d\n", n1, n2, Sum);
+// check if the sum is even or odd,
+// and print result to screen
+
+if(Sum % 2 == 0) 
+{
+	printf("%d is even\n", Sum);
+}
+else
+{	
+	printf("%d is odd\n", Sum);
+}
 return 0;
 }
